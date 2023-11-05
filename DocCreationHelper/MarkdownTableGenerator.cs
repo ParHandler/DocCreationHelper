ac05cbeb57a0ca7b4dfde691b8cce0daacae9501
Куниловский Евгений Георгиевич <Evgeniy.Kunilovskiy@home.bank>
2023-11-05 19:11:08 +0300

    Format MarkdownTableGenerator output

--- DocCreationHelper/MarkdownTableGenerator.cs
+++ DocCreationHelper/MarkdownTableGenerator.cs
@@ -50,9 +50,17 @@ public class MarkdownTableGenerator : ITableGenerator
             // Используем xmlDocExtractor для получения описания свойства из XML-документации
             var propertyDescription = _xmlDocExtractor.GetPropertySummary(fullPath);
 
+
+            // Split the propertyDescription into multiple lines with a maximum line length
+            const int maxLineLength = 30; // Adjust the line length as needed
+            var descriptionLines = SplitDescription(propertyDescription, maxLineLength);
+
+            // Combine the description lines with line breaks
+            var formattedDescription = string.Join($"\\n", descriptionLines);
+
             // Добавляем строку с информацией о свойстве в таблицу
             outputBuilder.AppendLine(
-                $"| {name} | {propertyTypeName} | {propertyIsNullable} | {propertyDescription} |");
+                $"| {name} | {propertyTypeName} | {propertyIsNullable} | {formattedDescription} |");
         }
 
         return outputBuilder.ToString();
@@ -61,5 +69,32 @@ public class MarkdownTableGenerator : ITableGenerator
     public string GenerateTableFromYaml(string yaml)
     {
         throw new NotImplementedException();
+    }
+
+    // Helper method to split a description string into lines
+    private IEnumerable<string> SplitDescription(string description, int maxLineLength)
+    {
+        var lines = new List<string>();
+        var currentLine = new StringBuilder();
+
+        foreach (var word in description.Split(' '))
+        {
+            if (currentLine.Length + word.Length + 1 <= maxLineLength)
+            {
+                if (currentLine.Length > 0)
+                    currentLine.Append(' ');
+                currentLine.Append(word);
+            }
+            else
+            {
+                lines.Add(currentLine.ToString());
+                currentLine = new StringBuilder(word);
+            }
+        }
+
+        if (currentLine.Length > 0)
+            lines.Add(currentLine.ToString());
+
+        return lines;
     }
 }
